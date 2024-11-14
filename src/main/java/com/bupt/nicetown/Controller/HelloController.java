c7a6b4cbd0417eb2e211dcc7e384874660323f8c
Kannshya <jp2021212716@qmul.ac.uk>
2024-11-14 19:13:37 +0800

    maven构建springboot项目，引入了mybatis的依赖但暂时先屏蔽了数据库。

---
+++ src/main/java/com/bupt/nicetown/Controller/HelloController.java
@@ -0,0 +1,14 @@
+package com.bupt.nicetown.Controller;
+
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class HelloController {
+    @RequestMapping("/hello")
+    public String hello() {
+//        System.out.println("hello Pada");
+        return "Hello World";
+    }
+
+}
---
+++ src/test/java/com/bupt/nicetown/NiceTownApplicationTests.java
@@ -0,0 +1,13 @@
+package com.bupt.nicetown;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class NiceTownApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
